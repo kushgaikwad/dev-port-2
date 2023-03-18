2039e9d2228a2187564a7730404aa5055a03cd02
Kush <kushalxgaikwad@gmail.com>
2023-03-18 14:44:23 +0530

    Added custom font

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,9 @@ export default function Home() {
       <Head>
         <title>Kush Gaikwad Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet"/>
 
       </Head>
 
