1c326229c72222968c435239f11a021d223d4c7c
Kush <kushalxgaikwad@gmail.com>
2023-04-07 20:31:30 +0530

    Hosted assets on cloudinary

--- components/BackgroundVideo.tsx
+++ components/BackgroundVideo.tsx
@@ -7,9 +7,7 @@ const BackgroundVideo = (props: Props) => {
         //  <div className={styles.videoBackground}>
         <div className='absolute top-0 left-0 w-full h-screen -z-10 object-covers brightness-50'>
             <video autoPlay loop muted className='object-cover w-full h-full'>
-                {/* <source src='https://drive.google.com/file/d/1A_fl7rHVldytH-lPJMp9qkciEtOmjD2f/view?usp=share_link4' type="video/mp4" /> */}
-                {/* <source src='https://www.youtube.com/watch?v=po02mFUhRTk' type="video/mp4" /> */}
-                <source src='/images/Kush-hero-video.mp4' type="video/mp4" />
+                <source src='https://res.cloudinary.com/dvszxda1u/video/upload/v1680879415/Kush-hero-video_xhocq2.mp4' type="video/mp4" />
             </video>
         </div>
     );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BackgroundVideo from './BackgroundVideo'
 import { motion } from "framer-motion"
 import Header from './Header'
+import Wrapper from './Wrapper'
 
 
 type Props = {}
@@ -9,32 +10,37 @@ type Props = {}
 function Hero({ }: Props) {
 
     return (
+
+
         <div className='relative h-screen'>
             <BackgroundVideo />
             <Header />
-            <motion.div initial={{
-                x: -500,
-                opacity: 0,
-                scale: 0.2
-            }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 1
-
+           
+                <motion.div initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.2
                 }}
-                transition={{
-                    duration: 1,
-                }}
-    
-                className="z-50 flex flex-col content-end w-full pr-8 text-[1.5rem] text-right text-white place-content-end h-3/4 text-[2.1rem] leading-[2.4rem] sm:text-[2.7rem] md:text-[3.5rem] lg:text-[4.3rem] lg:leading-[4.8rem] sm:leading-[3.5rem] md:leading-[4.3rem] lg:leading-[2rem] xl:text-[88px] xl:leading-[6rem] "
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        scale: 1
 
-            >
-                <h1>Hi.</h1>
-                <h1>I am Kush.</h1>
-                <h1>A Creative Technologist.</h1>
-            </motion.div>
+                    }}
+                    transition={{
+                        duration: 1,
+                    }}
+
+                    className="z-50 flex flex-col content-end w-full pr-8 text-[1.5rem] text-right text-white place-content-end h-3/4 text-[2.1rem] leading-[2.4rem] sm:text-[2.7rem] md:text-[3.5rem] lg:text-[4.3rem] lg:leading-[4.8rem] sm:leading-[3.5rem] md:leading-[4.3rem] lg:leading-[2rem] xl:text-[88px] xl:leading-[6rem] "
+
+                >
+                    <h1>Hi.</h1>
+                    <h1>I am Kush.</h1>
+                    <h1>A Creative Technologist.</h1>
+                </motion.div>
+           
         </div >
+
 
     )
 }
