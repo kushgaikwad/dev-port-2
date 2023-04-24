95a89c2acbbee1671f94fc71509f3584b30a9c54
Kush <kushalxgaikwad@gmail.com>
2023-04-24 15:49:06 +0530

    Added Projects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Libre_Baskerville } from 'next/font/google'
+
+const libreBaskerville = Libre_Baskerville({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-libre',
+  style: ['normal', 'italic'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  (<main className={`${libreBaskerville.variable} font-sans`}> <Component {...pageProps} /></main>)
+  
+    
+  
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet" /> */}
 
       </Head>
       <body>
